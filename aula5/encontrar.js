31545f4a28b5fa46ba7731f310b099850de2be4e
Analicesilva-27 <da.silva.analice@escola.pr.gov.br>
2026-06-01 08:34:35 -0300

    ordenar

---
+++ aula5/encontrar.js
@@ -0,0 +1,13 @@
+const cliente = require("./cliente.json");
+
+function encontrar(lista,chave,valor){
+    return lista.find((item) => item[chave] === valor);
+}
+
+const encontrado = encontrar(cliente,"nome","Kirby");
+
+const encontrado2 = encontrar(cliente, "telefone", "18977479960");
+
+console.log(encontrado);
+
+console.log(encontrado2);
